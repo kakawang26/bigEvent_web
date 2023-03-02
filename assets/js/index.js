5045dbfd311c08b86c04bf0df51b1ef5fd3b34f5
wangxia <18770913380@163.com>
2023-03-02 10:17:19 +0800

    完成主页功能

---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+$(function () {
+  getUserInfo();
+
+  $("#logout").on("click", function () {
+    layer.confirm(
+      "确定退出登录？",
+      { icon: 3, title: "提示" },
+      function (index) {
+        localStorage.removeItem("token");
+        location.href = "/login.html";
+        layer.close(index);
+      }
+    );
+  });
+});
+
+// 获取用户基本信息
+function getUserInfo() {
+  $.ajax({
+    method: "get",
+    url: "/my/userinfo",
+    success: function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message);
+      }
+      //  渲染用户信息
+      renderAvatar(res.data);
+    },
+  });
+}
+
+function renderAvatar(userinfo) {
+  var name = userinfo.nickname || userinfo.username;
+  $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+
+  if (userinfo.user_pic) {
+    $(".layui-nav-img").attr("src", userinfo.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    var first = name[0].toUpperCase();
+    $(".layui-nav-img").hide();
+    $(".text-avatar").html(first).show();
+  }
+}
